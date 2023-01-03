7abaf7a8e54a2bbd8322e5241dd49988ffad24c2
chelseadgls <chelseamdouglas@gmail.com>
2023-01-03 16:54:09 -0500

    changes

--- server.js
+++ server.js
@@ -17,7 +17,5 @@ app.use("/", routes);
 db.on("connected", () => {
   console.clear();
   console.log(chalk.blue("Connected to MongoDB!"));
-  app.listen(port, () => {
-    console.log(`server running on ${port}`);
-  });
+  app.listen(port, () => console.log(`app listening on port ${port}`))
 });
