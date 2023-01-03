9d87bd42c4660f748ec32ea30843573d200bf213
chelseadgls <chelseamdouglas@gmail.com>
2023-01-03 17:36:20 -0500

    port changed

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import logger from 'morgan';
 import chalk from 'chalk';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 
 app.use(express.json());
 app.use(cors());
