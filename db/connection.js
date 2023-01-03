f5837882b71861ce293525c3353b5ede6f6a20bb
chelseadgls <chelseamdouglas@gmail.com>
2023-01-03 17:01:38 -0500

    changes

--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,22 @@
 import mongoose from 'mongoose';
 import chalk from 'chalk';
-
-const url = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/showcase'
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 let mongooseConfig = {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 }
 
-mongoose
-  .set('strictQuery', false)
-  .connect(url, mongooseConfig)
-  .catch((error) => {
-    console.log(`Error connecting to MongoDB: ${error.message}`);
-  });
+mongoose.set("returnOriginal", false);
+
+const connectionString = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/showcase"
+
+  mongoose
+    .connect(connectionString, mongooseConfig)
+    .catch((error) => {
+      console.log(`Error connecting to MongoDB: ${error.message}`);
+    });
 
 mongoose.connection.on("disconnected", () => {
   console.log(chalk.bold("Disconnected from MongoDB!"));
--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-import db from "./db/connection.js";
-import routes from "./routes/index.js";
-import express from "express";
-import cors from "cors";
-import logger from "morgan";
-import chalk from "chalk";
+import db from './db/connection.js';
+import routes from './routes/index.js';
+import express from 'express';
+import cors from 'cors';
+import logger from 'morgan';
+import chalk from 'chalk';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
-app.use(logger("dev"));
+app.use(logger('dev'));
 
-app.use("/", routes);
+app.use('/', routes);
 
-db.on("connected", () => {
+db.on('connected', () => {
   console.clear();
-  console.log(chalk.blue("Connected to MongoDB!"));
-  app.listen(process.env.PORT);
+  console.log(chalk.blue('Connected to MongoDB'));
+  app.listen(PORT, () => {
+    console.log(`Express server running in development on http://localhost:${PORT}`);
+  });
 });
