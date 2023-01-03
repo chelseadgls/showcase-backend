93b56f9735273213dc275ec95883756fc6b22fbc
chelseadgls <chelseamdouglas@gmail.com>
2023-01-03 13:11:06 -0500

    creator model built

--- models/Creator.js
+++ models/Creator.js
@@ -1,9 +1,15 @@
-// import mongoose from 'mongoose'
-// const Schema = mongoose.Schema
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
 
-// const creatorSchema = new mongoose.Schema({
+const creatorSchema = new mongoose.Schema({
+id: { type: String },
+createdTime: { type: Date },
+fields: [{ 
+  Name: { type : String}.
+  Items: { type: Array},
+  Spaces: { type: Array},
+}]
+})
 
 
-// })
-
-// export default mongoose.model('Creator', creatorSchema)
+export default mongoose.model('Creator', creatorSchema)
--- seed/data.js
+++ seed/data.js
@@ -1,22 +1,59 @@
-import mongoose from '../db/connection.js';
-// import creatorData from '' assert { type: "json" }
-// import itemData from '' assert { type: "json" }
-// import spaceData from '' assert { type: "json" }
-import Creator from '../models/Creator.js'
-import Item from '../models/Item.js'
-import Space from '../models/Space.js'
+import axios from 'axios';
+import fs from 'fs';
+import db from '../db/connection.js';
+import Creator from '../models/Creator.js';
+// import Item from '../models/Item.js';
+// import Space from '../models/Space.js';
 
-// let structuredCreatorData = creatorData.map(
+let allCreators = {};
+let allItems = {};
+import allSpaces = {};
 
-// );
 
-// let structuredItemData = itemData.map(
+export const retrieveCreators = async () => {
+  try {
+    const response = await axios.get(
+      "https://eojbtbh68hot62m.m.pipedream.net"
+    );
+    // console.log(response.data);
+    let creatorsData = response.data;
+    let structuredCreatorsData = creatorsData.map(
+      ({}) => {
+        return {
 
-// );
+        };
+      }
+    );
+    allCreators = structuredCreatorsData;
+    writeCreatorData();
+  } catch (error) {
+    throw error;
+  }
+  };
 
-// let structureSpaceData = spaceData.map(
+  // export const retrieveItems = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       "https://eoc685b1ynuwbq.m.pipedream.net"
+  //     );
+  //     // console.log(response.data);
+  //     let itemsData = response.data;
+  //   } catch (error) {
+  //     throw error;
+  //   }
+  //   };
 
-// );
+  //   export const retrieveSpaces = async () => {
+  //     try {
+  //       const response = await axios.get(
+  //         "https://eozjtvk41svx4nc.m.pipedream.net"
+  //       );
+  //       // console.log(response.data);
+  //       let spacesData = response.data;
+  //     } catch (error) {
+  //       throw error;
+  //     }
+  //     };
 
 let insertData = async () => {
   await Creator.deleteMany({});
